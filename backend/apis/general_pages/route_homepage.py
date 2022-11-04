3c4f1022edb313ce891d94e2b45a4868b323bb3f
VikasR1 <vikasrajoria@outlook.com>
2022-11-04 10:09:48 +0530

    updated routhomepage.py

--- backend/apis/general_pages/route_homepage.py
+++ backend/apis/general_pages/route_homepage.py
@@ -9,11 +9,12 @@ from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="templates")
 general_pages_router = APIRouter()
 
-
+#end point or index page
 @general_pages_router.get("/")
 async def index(request: Request):
 	return templates.TemplateResponse("general_pages/index.html",{"request":request})
 
+#endpoint for home page
 @general_pages_router.get("/home")
 async def home(request: Request):
 	return templates.TemplateResponse("general_pages/homepage.html",{"request":request})

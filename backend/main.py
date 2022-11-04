4990b9eb42b1e527212829b76aa1fb7d1cf6c401
VikasR1 <vikasrajoria@outlook.com>
2022-11-04 10:02:05 +0530

    first commit

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+'''
+reference taken from
+
+https://www.fastapitutorial.com/blog/serving-html-fastapi/
+
+'''
+
+#main.py 
+
+from fastapi import FastAPI
+from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
+
+
+def include_router(app):
+	app.include_router(general_pages_router)
+
+
+def start_application():
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+	include_router(app)
+	return app 
+
+
+app = start_application()
